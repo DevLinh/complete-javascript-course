5923eeb0fe1bf3da47fcdc1c4ee24f698c60f3c3
devlinh <devlinh99qb@gmail.com>
2021-11-09 10:56:58 +0700

    16. Coding Challeng #1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -46,30 +46,45 @@
 ///////////////////////////
 //Basic Operators
 // Math operators
-const now = 2021;
-const myAge = now - 1999;
-console.log(myAge);
+// const now = 2021;
+// const myAge = now - 1999;
+// console.log(myAge);
 
-const myBrotherAge = now - 2004;
-console.log(myBrotherAge);
+// const myBrotherAge = now - 2004;
+// console.log(myBrotherAge);
 
-console.log(myAge*2, myAge/5, 2 ** 3);
-const firstName = 'Linh';
-const lastName = 'Vo Van';
-console.log(firstName + ' ' +lastName);
+// console.log(myAge*2, myAge/5, 2 ** 3);
+// const firstName = 'Linh';
+// const lastName = 'Vo Van';
+// console.log(firstName + ' ' +lastName);
 
-// Assignment operators
-let x = 10 + 5;
-x+=22;
-x-=10;
-x*=4;
-x++;
-x--;
-console.log(x);
+// // Assignment operators
+// let x = 10 + 5;
+// x+=22;
+// x-=10;
+// x*=4;
+// x++;
+// x--;
+// console.log(x);
 
-// Comparison operators
-console.log(myAge + 4 > 25);
-console.log(myBrotherAge >= myAge);
+// // Comparison operators
+// console.log(myAge + 4 > 25);
+// console.log(myBrotherAge >= myAge);
 
-const isFullAge = myAge >= 18;
-console.log(isFullAge);
+// const isFullAge = myAge >= 18;
+// console.log(isFullAge);
+
+///////////////////////////
+// Operator Precedence
+// console.log(now - 2004 > now - 1999);
+
+///////////////////////////
+// Coding Challenge #1
+const calculateBMI = (mass, height) => mass / (height**2);
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+let markHigherBMI = calculateBMI(markMass, markHeight) > calculateBMI(johnMass, johnHeight);
+console.log(markHigherBMI);
+
