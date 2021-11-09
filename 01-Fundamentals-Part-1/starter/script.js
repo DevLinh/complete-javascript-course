0ba6710720962baecda8ab619d74fa92f9f741e5
devlinh <devlinh99qb@gmail.com>
2021-11-09 10:19:10 +0700

    11. Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,27 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JS is Fun!');
-console.log(40 + 9 - 10 + 3);
+// let js = 'amazing';
+// if (js === 'amazing') alert('JS is Fun!');
+// console.log(40 + 9 - 10 + 3);
 
+///////////////////////////
+// Variable name conventions
+// let firstName = "Linh";
+// console.log(firstName);
 
-let firstName = "Linh";
-console.log(firstName);
+// let age = 23;
+// console.log(`Age: ${age}`);
 
-let age = 23;
-console.log(`Age: ${age}`);
+// let PI = 3.1415;
 
-let PI = 3.1415;
+// console.log("PI = " + PI);
 
-console.log("PI = " + PI);
+///////////////////////////
+//Date Types
+let isIsland = false;
+let population = 90000000;
+let country = 'Vietnam';
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
