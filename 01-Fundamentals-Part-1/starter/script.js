6bd2ef236f82676bfc39af73c4143d5a57eaca44
devlinh <devlinh99qb@gmail.com>
2021-11-09 10:07:59 +0700

    10. Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,14 @@
 let js = 'amazing';
 if (js === 'amazing') alert('JS is Fun!');
 console.log(40 + 9 - 10 + 3);
+
+
+let firstName = "Linh";
+console.log(firstName);
+
+let age = 23;
+console.log(`Age: ${age}`);
+
+let PI = 3.1415;
+
+console.log("PI = " + PI);
