00efbe03ae5d2dcb52b8ebdb93bb13e9204c7b83
devlinh <devlinh99qb@gmail.com>
2021-11-09 14:28:51 +0700

    16. Coding Challeng #4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -243,8 +243,30 @@ GOOD LUCK 😀
 
 ////////////////////////////////////
 // The Conditional (Ternary) Operator
-const age = 22;
-age >= 18 ? console.log('You can drink wine') : console.log('You can not drink wine');
+// const age = 22;
+// age >= 18 ? console.log('You can drink wine') : console.log('You can not drink wine');
 
-const validDrink = age >= 18 ? 'Beer' : 'Pepsi';
-console.log(validDrink);
+// const validDrink = age >= 18 ? 'Beer' : 'Pepsi';
+// console.log(validDrink);
+
+////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. 
+In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+*/
+
+const billValue = 275;
+const totalExpense = billValue <= 300 && billValue >= 50 ? billValue*(1 + 0.15) : billValue*(1 + 0.2);
+console.log(`You have to pay ${totalExpense}$`);
